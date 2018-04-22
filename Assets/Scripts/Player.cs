b9287eb4ea31c6219eb224d8ed91292a6c191c2e
PopaAdrianTesting <popaadrian18@yahoo.com>
2018-04-22 13:31:18 +0300

    Third Person Controller - base movement scripts, input controller, game manager and camera, all implemented at their basic form, required follow-up

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MoveController))]
+public class Player : MonoBehaviour {
+
+    [System.Serializable]
+    public class MouseInput
+    {
+        public Vector2 Damping;
+        public Vector2 Sensitivity;
+
+    }
+
+    [SerializeField] float speed;
+    [SerializeField] MouseInput MouseControl;
+
+    private MoveController m_moveController;
+    public MoveController MoveController
+    {
+        get
+        {
+            if (m_moveController == null)
+                m_moveController = GetComponent<MoveController>();
+            return m_moveController;
+        }
+
+    }
+    InputController playerInput;
+	void Awake () {
+        playerInput = GameManager.Instance.InputController;
+        GameManager.Instance.LocalPlayer = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 direction = new Vector2(playerInput.Vertical * speed, playerInput.Horizontal * speed);
+        MoveController.Move(direction);
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    Player localPlayer;
+	// Use this for initialization
+	void Awake () {
+        GameManager.Instance.OnLocalPlayerJoined += HandleOnLocalPlayerJoined;
+	}
+	
+    void HandleOnLocalPlayerJoined (Player player)
+    {
+        localPlayer = player;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
