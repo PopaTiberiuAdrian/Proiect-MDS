5b839b36ba2ada42186e3c1f1e1b8a4849a21cf6
PopaAdrianTesting <popaadrian18@yahoo.com>
2018-04-22 20:01:08 +0300

    Prefab with scripts for destroyable objects

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : Destructable {
+
+    public override void Die()
+    {
+        base.Die();
+        print("We die");
+    }
+    public override void TakeDamage(float amount)
+    {
+        print("Remaining: " + HitPointsRemaining);
+        base.TakeDamage(amount);
+    }
+}
