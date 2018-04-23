0fccc355779ea7eb6ceb6d6bfa3bdf5d4c6a6a32
PopaAdrianTesting <popaadrian18@yahoo.com>
2018-04-23 16:40:18 +0300

    All sided animations added, the template for player movement is finished

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Vertical", GameManager.Instance.InputController.Vertical);
+        animator.SetFloat("Horizontal", GameManager.Instance.InputController.Horizontal);
+    }
+}
