3aaaf9c74fb3aad1111b9b9b16518f89390f2ae3
anca6991 <ancuta.vaduva@my.fmi.unibuc.ro>
2018-04-25 00:45:11 +0300

    Script for Minimap

---
+++ Assets/Scripts/Minimap/Minimap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+
+    [SerializeField]Transform playerTransform;
+    Vector3 playerPositionAuxilliary;
+    private void LateUpdate()
+    {
+        transform.rotation = Quaternion.Euler(90f, playerTransform.eulerAngles.y, 0f);
+        playerPositionAuxilliary.x = playerTransform.position.x;
+        playerPositionAuxilliary.z = playerTransform.position.z;
+        playerPositionAuxilliary.y = transform.position.y;
+        transform.position = playerPositionAuxilliary;
+    }
+}
