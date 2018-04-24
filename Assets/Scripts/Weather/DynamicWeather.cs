7a65f6e17e2420479df14ea4caf015b41a39d97a
anca6991 <ancuta.vaduva@my.fmi.unibuc.ro>
2018-04-24 23:46:24 +0300

    Basic weather configuration with time management and skybox blend

---
+++ Assets/Scripts/Weather/DynamicWeather.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicWeather : MonoBehaviour {
+
+    [SerializeField] float weatherDurationTime = 20f;
+    [SerializeField] float weatherStartTime;
+    [SerializeField] GameObject rainPrefab;
+    int timeOfDay;
+    float timer = 12;
+    float skyboxBlendRange = 0.0f;
+    bool doIncrease = true;
+	public enum WeatherPhases
+    {
+        SunnyWeather,
+        RainyWeather
+    }
+
+    public void SunnyWeather()
+    {
+        rainPrefab.SetActive(false);
+    }
+
+    public void RainyWeather()
+    {
+        rainPrefab.SetActive(true);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        timeOfDay = Mathf.RoundToInt(timer % 60);
+
+        if (skyboxBlendRange >= 1.0f)
+            doIncrease = false;
+        if (skyboxBlendRange <= 0.0f)
+            doIncrease = true;
+
+        if (doIncrease == true)
+        {
+            skyboxBlendRange += 0.001f;
+            //SunnyWeather();
+        }
+        else
+        {
+            skyboxBlendRange -= 0.001f;
+            //RainyWeather();
+        }
+        
+        RenderSettings.skybox.SetFloat("_Blend", skyboxBlendRange);
+    }
+}
