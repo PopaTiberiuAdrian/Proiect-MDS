2da572e80652981753c1d427128a09fcbca822b0
unknown <ancuta.vaduva@my.fmi.unibuc.ro>
2018-06-04 23:55:02 +0300

    Unit tests and waypoint mesh render deactivation

---
+++ Assets/Scripts/Player/PlayerStatsTests.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+[TestFixture]
+public class PlayerStatsTests {
+
+    [Test]
+    public void PlayerBeginsWithMinMoneyTest()
+    {
+        const int minAmountOfMoney = 0;
+        PlayerStats playerStats = new PlayerStats();
+        Assert.That(playerStats.getAmountOfMoney(), Is.EqualTo(minAmountOfMoney));
+    }
+
+    [TestCase(20, 9999,9999)]
+    [TestCase(0, 9999,9999)]
+    public void PlayerAmountOfMoneyNeverPassLimitTest(int moneyThatIncrease, int amountOfMoney, int moneyExpected)
+    {
+        PlayerStats playerStats = new PlayerStats(amountOfMoney);
+        playerStats.increaseAmountOfMoney(moneyThatIncrease);
+        Assert.That(playerStats.getAmountOfMoney(), Is.EqualTo(moneyExpected));
+    }
+
+    [TestCase(20, 0)]
+    [TestCase(0, 0)]
+    public void PlayerAmountOfMoneyNeverNegativeTest(int moneyThatDecrease, int moneyExpected)
+    {
+        PlayerStats playerStats = new PlayerStats();
+        playerStats.decreaseAmountOfMoney(moneyThatDecrease);
+        Assert.That(playerStats.getAmountOfMoney(), Is.EqualTo(moneyExpected));
+    }
+
+    [TestCase(20, 5, 0, 0, 0)]
+    [TestCase(20, 5, 20, 5, 15)]
+    [TestCase(20, 5, 20, 10, 10)]
+    public void RoundsRemainingInClipTest(int maxAmmo, float reloadTime, int clipSize, int shotsFiredInClip, int roundsRemainingInClip)
+    {
+        WeaponReloader weaponReloader = new WeaponReloader(maxAmmo,reloadTime,clipSize);
+        weaponReloader.shotsFiredInClip = 0;
+        weaponReloader.TakeFromClip(shotsFiredInClip);
+        Assert.That(clipSize - weaponReloader.shotsFiredInClip, Is.EqualTo(roundsRemainingInClip));
+    }
+
+    [Test]
+    public void PlayerAmountOfMoneyOppositeValuesTest(int oppositeNumber)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+   
+    public bool WaypointMesh(bool enableDisable)
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = enableDisable;
+        return enableDisable;
+    }
+	
+	void Start () {
+        WaypointMesh(false);
+	}
+	
+}
