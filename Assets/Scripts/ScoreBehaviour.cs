a6b6345bb208d1df2bf0a0672c04ee6a36bf1e63
unknown <ancuta.vaduva@my.fmi.unibuc.ro>
2018-06-04 15:49:12 +0300

    Score Behaviour

---
+++ Assets/Scripts/ScoreBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private int value;
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, 50 * Time.deltaTime); //rotates 50 degrees per second around z axis
+    }
+}
