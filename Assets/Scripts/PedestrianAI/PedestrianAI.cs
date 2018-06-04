998bcd34653702dbff9909772ad7b1ef499ef4be
PopaAdrianTesting <popatiberiuadrian@gmail.com>
2018-06-04 14:25:44 +0300

    Pedestrian AI added

---
+++ Assets/Scripts/PedestrianAI/PedestrianAI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianAI : MonoBehaviour {
+
+    public Transform player;
+    public Transform head;
+    static Animator anim;
+
+    string state = "patrol";
+    public GameObject[] waypoints;
+    int currentWP = 0;
+    float rotSpeed = 2.2f;
+    float speed = 1.5f;
+    float accuracyWP = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = player.position - this.transform.position;
+        direction.y = 0;
+        float agnel = Vector3.Angle(direction, head.up);
+
+        if(state == "patrol" && waypoints.Length > 0)
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
+            if(Vector3.Distance(waypoints[currentWP].transform.position, transform.position) < accuracyWP)
+            {
+                currentWP++;
+                if(currentWP >= waypoints.Length)
+                {
+                    currentWP = 0;
+                }
+            }
+
+            direction = waypoints[currentWP].transform.position - transform.position;
+            this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+            this.transform.Translate(0, 0, Time.deltaTime * speed);
+        }
+	}
+}
