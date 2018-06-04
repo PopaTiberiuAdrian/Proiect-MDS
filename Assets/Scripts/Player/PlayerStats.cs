982f6113de7c9d07ccbba9a54b410a56acebd42f
unknown <vaduva.ancuta@my.fmi.unibuc.ro>
2018-06-04 15:33:47 +0300

    PlayerStats

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    private int amountOfMoney;
+
+    public PlayerStats()
+    {
+        amountOfMoney = 0;
+    }
+
+    public PlayerStats(int amountOfMoneyDefault)
+    {
+        amountOfMoney = amountOfMoneyDefault;
+    }
+
+    public int getAmountOfMoney()
+    {
+        return amountOfMoney;
+    }
+
+    public bool increaseAmountOfMoney(int add)
+    {
+        amountOfMoney += add;
+        return true;
+    }
+
+    public bool decreaseAmountOfMoney(int dec)
+    {
+        if (amountOfMoney - dec < 0)
+            return false;
+        amountOfMoney -= dec;
+        return true;
+    }
+
+}
